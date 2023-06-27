7a41b7e17112017b61ad5672e3e7f837be81dec3
Kristina <mary312ti@gmail.com>
2023-06-27 22:15:20 +0300

    SearchHints

---
+++ src/components/Header/Search/SearchResultList.vue
@@ -0,0 +1,29 @@
+<template>
+    <ul class="absolute top-12 rounded-lg drop-shadow-lg bg-white py-2">
+        <li v-for="(result, index) in searchResult" :key="result" :class="listItemClasses(index)">
+            <BaseIcon name="search" strokeColor="currentColor" fillColor="none" class="w-5 h-5"></BaseIcon>
+            {{ result }}
+        </li>
+    </ul>
+</template>
+
+<script setup>
+import BaseIcon from '../../Base/BaseIcon.vue';
+
+const searchResult = ['lorem ipsum', 'lorem ipsum1', 'lorem ipsum2', 'lorem ipsum3', 'lorem ipsum4'];
+
+const listItemClasses = (index) => {
+    const classes = ['hover:bg-gray-100', 'px-12', 'py-2', 'flex', 'flex-row', 'gap-4',];
+
+    if (index === 0) {
+        classes.push('mt-4');
+    }
+
+    if (index === searchResult.length - 1) {
+        classes.push('mb-4');
+    }
+
+    return classes;
+};
+
+</script>
--- src/components/Header/Search/TheSearchInput.vue
+++ src/components/Header/Search/TheSearchInput.vue
@@ -1,4 +1,24 @@
 <template>
-    <input type="text" placeholder="Search"
-        class="bg-white w-full px-3 shadow-inner rounded-bl-3xl rounded-tl-3xl border border-gray-300 focus:outline-none placeholder:text-gray-500" />
+    <input type="text" @click="toggleList" placeholder="Search"
+        class="relative bg-white w-full px-3 shadow-inner rounded-bl-3xl rounded-tl-3xl border border-gray-300 focus:outline-none placeholder:text-gray-500" />
+        <SearchResultList  v-if="showList" />
 </template>
+
+<script setup>
+import SearchResultList from './SearchResultList.vue';
+import { ref, } from 'vue';
+
+// const { inputWidth } = toRefs(props);
+
+const showList = ref(false);
+
+const toggleList = () => {
+    showList.value = !showList.value;
+};
+
+// onMounted(() => {
+//   const input = document.querySelector('input');
+//   inputWidth.value = `${input.offsetWidth}px`;
+// });
+
+</script>
