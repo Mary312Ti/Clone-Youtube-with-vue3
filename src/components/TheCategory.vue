863543450417b02791b7191ec9691882f0422cd8
Kristina <mary312ti@gmail.com>
2023-07-15 00:10:28 +0300

    TheCategory with youtube data

--- src/components/TheCategory.vue
+++ src/components/TheCategory.vue
@@ -2,49 +2,37 @@
   <section :class="marginClasses" class="flex fixed bg-white w-screen xs:top-14 z-20 pl-4">
     <div class="max-w-screen-2xl m-auto">
       <div class="scrollbar scrollbar-none text-sm py-5 pl-4 bg-white flex space-x-3 overflow-auto whitespace-nowrap">
-       <CategoryItem
-       v-for="category in categories" :key="category" :category="category" :isActive="category === 'All'" />
+        <CategoryItem
+          v-for="category in videoCategories"
+          :key="category"
+          :category="category"
+          :isActive="category === 'All'"
+        />
       </div>
     </div>
   </section>
 </template>
 
 <script setup>
-
 import CategoryItem from './CategoryItem.vue';
-import { computed } from '@vue/reactivity';
+import { computed, onMounted } from 'vue';
+import { useGetCategories } from '../stores/getCategories';
 
 const props = defineProps({
   sidebarState: String
 });
 
 const marginClasses = computed(() => {
-    return {
-        'ml-64': props.sidebarState === 'normal',
-        'ml-24': props.sidebarState === 'compact'
-    }
+  return {
+    'ml-64': props.sidebarState === 'normal',
+    'ml-24': props.sidebarState === 'compact'
+  }
 });
 
+const store = useGetCategories();
+const videoCategories = computed(() => store.videoCategories.map(item => item.snippet.title));
 
-const categories = [
-  'All',
-  'Music',
-  'Playlists',
-  'Live',
-  'Gaming',
-  'Television comedy',
-  'Test drives',
-  'Animated films',
-  'History',
-  'Cars',
-  'Home improvement',
-  'Conversation',
-  'Pop music',
-  'Strategy video games',
-  'Role-Playing Games',
-  'Reently uploaded',
-
-]
-
-defineExpose({ categories })
+onMounted(() => {
+  store.fetchVideoCategories();
+});
 </script>
